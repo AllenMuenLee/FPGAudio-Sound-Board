267eb7c4f320d681d54b911f52647920dd497930
Allen Lee <limuen.allen@gmail.com>
2026-02-19 16:18:09 -0500

    noise gate code update

--- python logic check/Noise gate logic check.py
+++ python logic check/Noise gate logic check.py
@@ -1,14 +1,49 @@
-import numpy as np
+import sounddevice as sd
 from scipy.io import wavfile
+import numpy as np
 
-# 1. Load your audio
-fs, data = wavfile.read('input.wav')
-# Normalize to 16-bit range (-32768 to 32767)
-audio_in = data.astype(np.int16)
+def record_raw_audio(filename=r"C:\Users\limue\Documents\ASIC Hackathon\python logic check\input.wav", duration=5, fs=44100):
+    print(f"Recording for {duration} seconds")
+    
+    recording = sd.rec(int(duration * fs), samplerate=fs, channels=1, dtype='int16')
+    
+    sd.wait()
+    print("Recording finished")
+    wavfile.write(filename, fs, recording)
 
-# 2. Apply your Verilog logic
-threshold = 2048
-processed_audio = np.where(np.abs(audio_in) > threshold, audio_in, 0)
 
-# 3. Save to hear the result
-wavfile.write('output_filtered.wav', fs, processed_audio.astype(np.int16))
+def apply_noise_gate(input_file, output_file):
+    fs, data = wavfile.read(input_file)
+    audio_in = data.astype(np.int16)
+    processed_audio = np.zeros_like(audio_in)
+
+    open_threshold = 1000
+    close_threshold = 500
+    
+    is_open = False
+    
+    gain = 0.01
+    floor_gain = 0.01
+    atk_step, rls_step = 0.01, 0.01
+
+    for i in range(len(audio_in)):
+        sample = audio_in[i]
+        
+        abs_v = abs(sample)
+        if abs_v > open_threshold:
+            is_open = True
+        elif abs_v < close_threshold:
+            is_open = False
+            
+        if is_open:
+            gain = min(1.0, gain + atk_step)
+        else:
+            gain = max(floor_gain, gain - rls_step)
+
+        processed_audio[i] = sample * gain
+
+    final_audio = np.clip(processed_audio, -32768, 32767).astype(np.int16)
+    wavfile.write(output_file, fs, final_audio)
+    print("Noise gate applied")
+record_raw_audio()
+apply_noise_gate(r'C:\Users\limue\Documents\ASIC Hackathon\python logic check\input.wav', r'C:\Users\limue\Documents\ASIC Hackathon\python logic check\clean_output.wav')
