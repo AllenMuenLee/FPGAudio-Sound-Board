6d731025bdf06e7a6b14a7bdf5b38ae9b177ce77
Allen Lee <limuen.allen@gmail.com>
2026-02-21 14:58:20 -0500

    hz is not a variable mb

--- src/effect_highpitch.v
+++ src/effect_highpitch.v
@@ -49,7 +49,7 @@ module DIG_RAMDualAccess
     // CAUTION: uses distributed RAM
     reg [(Bits-1):0] memory [0:((1 << AddrBits)-1)];
 
-    assign \1D = ld? memory[\1A ] : 16'(hz);
+    assign \1D = ld? memory[\1A ] : 16'hz;
     assign \2D = memory[\2A ];
 
     always @ (posedge C) begin
